1717bdd6f5d3ad8eef60c12301779e95a8ea653c
Max Aalto <maxaalto@gmail.com>
2023-11-21 13:44:50 -0500

    building out some helper tools

--- README.md
+++ README.md
@@ -2,3 +2,21 @@
 
 ## Documentation 
 - Document how to install, set up QE and point to the pw.x executable
+
+## Supercloud 
+- Build QE from source (Github)
+- Include the following in .jupyter/llsc_notebook_bashrc:
+    export PYTHONNOUSERSITE=True
+    export PYTHONPATH=/home/gridsan/$USER/.conda/envs/$ENV/bin/python
+    export PATH="${PATH}:/home/gridsan/$USER/.conda/envs/$ENV/bin"
+- Launch Jupyter notebook with as many cores as desired
+- Create .env file with PWSCF_COMMAND and PSEUDOPOTENTIALS pointing to the correct files/directories
+- Run this at the top of the notebook:
+    os.environ['SLURM_TASKS_PER_NODE'] = os.environ['SLURM_CPUS_PER_TASK']
+    os.environ['SLURM_CPUS_PER_TASK'] = "1"
+
+What the fuck:
+The application appears to have been direct launched using "srun",
+but OMPI was not built with SLURM's PMI support and therefore cannot
+execute. There are several options for building PMI support under
+SLURM, depending upon the SLURM version you are using:
---
+++ alchemist/__init__.py
@@ -0,0 +1,45 @@
+import subprocess
+from sshconf import read_ssh_config
+from os.path import expanduser
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(prog="Alchemist")
+    subparsers = parser.add_subparsers(dest="command")
+
+    # Define a subcommand
+    start_parser = subparsers.add_parser("start")
+    start_parser.add_argument("--port", help="port to open jupyter server on", default=8080)
+    start_parser.add_argument("--token", help="jupyter token", default="alchemst")
+
+    connect_parser = subparsers.add_parser("connect")
+    connect_parser.add_argument("host", type=str, help="ssh hostname of remote server")
+    connect_parser.add_argument("--port", help="port to connect to remote jupyter server", default="8080")
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    if args.command == "start":
+        launch_server(args)
+
+    if args.command == "connect":
+        connect(args)
+
+def launch_server(args):
+    port = args.port
+    token = args.token
+    command = ["jupyter", "notebook", "--port={port}".format(port=port), "--NotebookApp.token={token}".format(token=token), "--no-browser"]
+    subprocess.Popen(command)
+
+def connect(args):
+    port = args.port
+    host = args.host
+    c = read_ssh_config(expanduser("~/.ssh/config"))
+    if host not in c.hosts():
+        raise ValueError("Host not found in ssh config file")
+    else: 
+        command = ['ssh', '-L', '{port}:localhost:{port}'.format(port=port), host]
+        subprocess.Popen(command)
+
+if __name__ == "__main__":
+    main()
